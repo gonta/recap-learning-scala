1c3dc560f340193715d6fbc965e0c5e516ff7fd7
sakamoto <sakamoto@bizreach.co.jp>
2015-02-10 10:13:43 +0900

    FizzBuzzed

--- src/main/scala/com/github/tamaki/study/tamaki/FizzBuzz/FizzBuzz.scala
+++ src/main/scala/com/github/tamaki/study/tamaki/FizzBuzz/FizzBuzz.scala
@@ -5,6 +5,18 @@ package com.github.tamaki.study.tamaki.FizzBuzz
  */
 object FizzBuzz {
   def main(args: Array[String]): Unit = {
+    (1 to 100).foreach(println)// 1 -> 100が出る
 
+    (1 to 100).foreach(x => fizzBuzz(x))
+
+    def fizzBuzz(s:Int){
+      val x:Any = {
+        if( s%15 == 0 ) "FizzBuzz"
+        else if( s%3 == 0 ) "Fizz"
+        else if( s%5 == 0 ) "Buzz"
+        else s
+      }
+      println(x)
+    }
   }
 }
